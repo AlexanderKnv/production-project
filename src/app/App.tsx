cf5f367a08a25fff9cac8f986ecda6b8050bfa97
Konaschkov <konashkov23@gmail.com>
2023-07-12 18:43:32 +0200

    Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,8 @@
-import { Route, Routes } from 'react-router-dom';
-import { Suspense } from 'react';
 import './styles/index.scss';
-import { Link } from 'react-router-dom';
 import {classNames} from "shared/lib/classNames/classNames";
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 
 const App = () => {
@@ -14,10 +10,9 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
