eedc73acb3fc5921fa47c9024ea8f62ca67bf48e
Konaschkov <konashkov23@gmail.com>
2023-07-16 10:58:42 +0200

    Internationalization. Plugin for translations

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import './styles/index.scss';
 import {classNames} from "shared/lib/classNames/classNames";
 import { useTheme } from 'app/providers/ThemeProvider';
+import {Suspense} from 'react';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 import {Sidebar} from "widgets/Sidebar";
@@ -10,11 +11,13 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Navbar />
-            <div className="content-page">
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback="">
+                <Navbar />
+                <div className="content-page">
+                    <Sidebar />
+                    <AppRouter />
+                </div>
+            </Suspense>
         </div>
     );
 };
