d311ef15516d98f9b414bfa82d163aff42608172
Konaschkov <konashkov23@gmail.com>
2023-09-03 19:29:19 +0200

    Upgrade sidebar, add button themes

--- src/shared/ui/LangSwitcher/LangSwitcher.tsx
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -1,26 +1,27 @@
 import {classNames} from "shared/lib/classNames/classNames";
 import cls from './LangSwitcher.module.scss';
 import {useTranslation} from "react-i18next";
-import {Button, ThemeButton} from "shared/ui/Button/Button";
+import {Button, ButtonTheme} from "shared/ui/Button/Button";
 
 interface LangSwitcherProps {
     className?: string;
+    short?: boolean;
 }
 
-export const LangSwitcher = ({className}: LangSwitcherProps) => {
+export const LangSwitcher = ({ className, short }: LangSwitcherProps) => {
     const { t, i18n } = useTranslation();
 
     const toggle = async () => {
-        i18n.changeLanguage(i18n.language === 'en' ? 'de' : 'en');
+        i18n.changeLanguage(i18n.language === 'de' ? 'en' : 'de');
     }
 
     return (
         <Button
-            className={classNames(cls.LangSwitcher, {}, [className])}
-            theme={ThemeButton.CLEAR}
+            className={classNames('', {}, [className])}
+            theme={ButtonTheme.CLEAR}
             onClick={toggle}
         >
-            {t('Language')}
+            {t(short ? 'En' : 'Language')}
         </Button>
     );
 };
